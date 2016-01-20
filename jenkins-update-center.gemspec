ae68bcb23c5753e6a7048461859e0533154bba3a
tse <markus@herrthese.de>
2016-01-20 14:29:51 +0100

    [update-center] #rename #add plugin and jenkins feature

---
+++ jenkins-update-center.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'jenkins-update-center'
+  s.version     = '0.0.2'
+  s.date        = '2016-01-20'
+  s.summary     = "jenkins update center"
+  s.description = "returns the current version of jenkins or given jenkins-plugin name"
+  s.authors     = ["Markus These"]
+  s.email       = 'markus@tse-c.de'
+  s.files       = ["lib/jenkins-update-center.rb"]
+  s.homepage    =
+      'https://github.com/herrthese/jenkins-update-center/blob/master/jenkins-update-center-0.0.2.gem'
+  s.license       = 'MIT'
+end
---
+++ lib/jenkins-update-center.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'net/http'
+
+class JenkinsUpdateCenter
+  UPDATE_CENTER = 'https://updates.jenkins-ci.org/current/update-center.json'
+
+  def self.validate_update_center
+    resp = Net::HTTP.get_response(URI.parse(UPDATE_CENTER))
+    data = resp.body.gsub(/updateCenter\.post\(\n/,'').gsub(/\n\);/,'')
+    JSON.parse(data)
+  end
+
+  def self.latest_informations
+    validate_update_center
+  end
+
+  def self.latest_jenkins_informations
+    jenkins = ''
+    validate_update_center.each do |k,v|
+      if k == 'core'
+        jenkins = v
+      end
+    end
+    jenkins
+  end
+
+  def self.latest_plugin_informations(name)
+    plugin = ''
+    validate_update_center.each do |k,v|
+      if k == 'plugins'
+        plugin = v[name]
+      end
+    end
+    plugin
+  end
+
+  def self.healthcheck
+    puts 'eat healthy'
+  end
+end
